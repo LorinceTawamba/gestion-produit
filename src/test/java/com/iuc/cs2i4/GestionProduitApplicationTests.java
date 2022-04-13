7a94b5b8e3f3d2462e58a182994ca33fa449ce9a
LorinceTawamba <lorince.tawamba@gmail.com>
2022-04-13 11:05:17 +0100

    Initial commit

---
+++ src/test/java/com/iuc/cs2i4/GestionProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuc.cs2i4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProduitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
